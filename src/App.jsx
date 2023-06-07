44e4183f6b409bed56bd70286f0bb64e0a854b77
adelson <tricker_bboy23@hotmail.com>
2023-06-06 22:26:58 -0400

    adding logo and title name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { Container, NextUIProvider } from '@nextui-org/react';
+import { Container, NextUIProvider, Image } from '@nextui-org/react';
 import { Text } from "@nextui-org/react";
 import "./App.css"
 import InvoicePage from './components/InvoicePage';
+
 
 function App() {
  
@@ -9,6 +10,11 @@ function App() {
   return (
     <NextUIProvider>
       <Container fluid justify='center' align='center' className='banner'>
+        <Image
+          width={75}
+          height={75}
+          src="../public/JRI.png"
+        />
         <Text h1 >JR Upholstery</Text>
       </Container>
       <InvoicePage></InvoicePage>
